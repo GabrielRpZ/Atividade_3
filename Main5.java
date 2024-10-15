8c349ed623a615cc3efa9ba8918fdb27224e6086
GabrielRpZ <gabrieljardim049@gmail.com>
2024-10-15 14:41:25 -0300

    Update Main5.java

--- Main5.java
+++ Main5.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-
+//atividade 5 e 6
 class Main5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
